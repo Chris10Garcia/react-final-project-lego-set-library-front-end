d2cb9076306361b6c79a888591d743043ef15a53
Christian Garcia <bones192nd@gmail.com>
2023-04-25 11:39:31 -0400

    create component list placeholder within app. mess around with UI css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
 // import './App.css';
 import React from 'react'
-import { Header } from 'semantic-ui-react';
+import { Header as HeaderUI, Menu as MenuUI, Container} from 'semantic-ui-react';
+import { NavLink, Route, Switch} from 'react-router-dom';
+
+function Header (){
+
+
+
+  return(
+    <Container text>
+      <HeaderUI as = 'h1'>Lego Set Library</HeaderUI>
+      <MenuUI inverted>
+        <MenuUI.Item 
+          as = {NavLink}
+          exact to = "/"
+          name = "home"
+        />
+        <MenuUI.Item 
+          as = {NavLink}
+          exact to = "/sets"
+          name = "set library"
+        />
+        <MenuUI.Item 
+          as = {NavLink}
+          exact to = "/add"
+          name = "add to library"
+        />
+      </MenuUI>
+    </Container>
+  )
+}
+
+function Home (){
+  return(
+    <HeaderUI as = 'h2'>Home Page</HeaderUI>
+  )
+}
+
+function BrickContainer (){
+  return(
+    <HeaderUI as = 'h2'>Lego Set Library Container</HeaderUI>
+  )
+}
+
+function NewBrickSet (){
+  return(
+    <HeaderUI as = 'h2'>Add new lego set here</HeaderUI>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <Header as = 'h1'> Testing this out </Header>
+      <Header/>
+      <Switch>
+        <Route exact path = "/">
+          <Home />
+        </Route>
+        <Route exact path = "/sets">
+          <BrickContainer />
+        </Route>
+        <Route exact path = "/add">
+          <NewBrickSet />
+        </Route>
+      </Switch>
+
     </div>
   );
 }
