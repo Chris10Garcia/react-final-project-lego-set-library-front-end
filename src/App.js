e86f32290ae9294725acf75b67663a9173ebb113
Christian Garcia <bones192nd@gmail.com>
2023-04-25 15:14:49 -0400

    figure out how to create scroll effect for static list of legos

--- src/App.js
+++ src/App.js
@@ -2,14 +2,45 @@
 import React from 'react'
 import { Route, Switch} from 'react-router-dom';
 import { 
-  Header as HeaderUI} from 'semantic-ui-react';
+  Container,
+  Grid,
+  Header as HeaderUI, Menu, Segment} from 'semantic-ui-react';
 import Header from './Header';
 import Home from './Home';
 
 
 function BrickContainer (){
   return(
-    <HeaderUI as = 'h2'>Lego Set Library Container</HeaderUI>
+    <Segment padded>
+      <Container textAlign='center'>
+        <HeaderUI as = 'h2'>Lego Set Library Container</HeaderUI>
+      </Container>
+      <Grid padded divided grid>
+        <Grid.Row>
+          <Grid.Column width = {4}>
+            <HeaderUI as = "h3">Leg Set List</HeaderUI>
+              <Menu vertical text fluid style = {{"overflow-y":"scroll", "max-height": "60vh"}}>
+                <Menu.Item name='Set 1'></Menu.Item>
+                <Menu.Item name='Set 1'></Menu.Item>
+                <Menu.Item name='Set 1'></Menu.Item>
+                <Menu.Item name='Set 1'></Menu.Item>
+                <Menu.Item name='Set 1'></Menu.Item>
+                <Menu.Item name='Set 1'></Menu.Item>                
+                <Menu.Item name='Set 1'></Menu.Item>
+                <Menu.Item name='Set 1'></Menu.Item>
+              </Menu>
+          </Grid.Column>
+          <Grid.Column width = {6}>
+            <Container text>
+              <HeaderUI as = "h3">Lego Set Details</HeaderUI>
+               
+              Lego details are placed here
+            
+            </Container>
+            </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   )
 }
 
